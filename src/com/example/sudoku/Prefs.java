1c8c980d7c841b59c7844c1b325b869d3fb0dd24
Myeongseok Baek <msbaek@daumcorp.com>
2012-12-24 19:20:44 +0900

    add settings
    res/xml/settings.xml 리소스 추가로 코드 변경 거의 없이 정의 가능
    AndroidManifest.xml에 Prefs 액티비티를 등록해야함.

--- src/com/example/sudoku/Prefs.java
+++ src/com/example/sudoku/Prefs.java
@@ -1,5 +1,12 @@
 package com.example.sudoku;
 
-public class Prefs {
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
+public class Prefs extends PreferenceActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings);
+	}
 }
