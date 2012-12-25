ac58704394223dad428c18a7a30d265d4deb21ae
Myeongseok Baek <msbaek@daumcorp.com>
2012-12-25 21:32:11 +0900

    declare PuzzleView class
    xml layout을 사용하지 않고 java로만 해 본다.
    android view의 크기는 생성 시점에는 알수 없다(0이다).
    실제 크기는 화면 생성 후 아무것도 드리지 않은 레이아웃 단계에서 계산됨.
    값이 계산되었을 때 onSizeChanged 메소드를 이용하거나 나중에 onDraw 메소드 같은 곳에서 getWidth, getHeight 메소드를 이용할 수 있다.

--- src/com/example/sudoku/PuzzleView.java
+++ src/com/example/sudoku/PuzzleView.java
@@ -1,10 +1,36 @@
 package com.example.sudoku;
 
 import android.content.Context;
+import android.graphics.Rect;
+import android.util.Log;
 import android.view.View;
 
 public class PuzzleView extends View {
+	private static final String TAG = "Sudoku";
+	private final Game game;
+	private float width; // 한 칸의 폭(width of one tile);
+	private float height; // 한 칸의 높이(height of one tile);
+	private int selX; // 선택된 것의 X 인덱스(X index of selection)
+	private int selY; // 선택된 것의 Y 인덱스(Y index of selection)
+	private final Rect selRect = new Rect();
+
 	public PuzzleView(Context context) {
 		super(context);
+		this.game = (Game) context;
+		setFocusable(true);
+		setFocusableInTouchMode(true);
+	}
+	
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		width = w / 9f;
+		height = h / 9f;
+		getRect(selX, selY, selRect);
+		Log.d(TAG, "onSizeChanged: width " + width + ", height " + height); 
+		super.onSizeChanged(w, h, oldw, oldh);
+	}
+
+	private void getRect(int x, int y, Rect rect) {
+		rect.set((int)(x * width), (int)(y * height), (int)(x * width + width), (int)(y * height + height));
 	}
 }
